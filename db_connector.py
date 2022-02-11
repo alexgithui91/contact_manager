c7fd112043dad707ece554df39d0935934b1ff27
alexgithui91 <alexgithui91@gmail.com>
2022-02-11 15:51:42 +0300

    working db connector

---
+++ db_connector.py
@@ -0,0 +1,17 @@
+import psycopg2
+from config import config
+
+
+def connect():
+    """Connect to PostgreSQL DB Server"""
+    conn = None
+
+    try:
+        # read connection params
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    return conn
