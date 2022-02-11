e5f9f81039e779300424f3a2cf1f10b390d2ff10
alexgithui91 <alexgithui91@gmail.com>
2022-02-11 16:04:08 +0300

    changed it return cursor instead of conn

--- db_connector.py
+++ db_connector.py
@@ -11,7 +11,8 @@ def connect():
         params = config()
         # connect to the PostgreSQL server
         conn = psycopg2.connect(**params)
+        cur = conn.cursor()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
-    return conn
+    return cur
